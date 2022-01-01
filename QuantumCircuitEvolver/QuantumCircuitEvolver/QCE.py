6c04304e37bf426a2c63e9860c505f7c498824a2
Sebastian Testanière Overksott <overskotts@gmail.com>
2022-01-01 22:20:38 +0100

    Moved code for running and finding error into classes

--- QuantumCircuitEvolver/QuantumCircuitEvolver/QCE.py
+++ QuantumCircuitEvolver/QuantumCircuitEvolver/QCE.py
@@ -1,37 +1,54 @@
-import quantum_circuit_evolver
+import quantum_circuit_evolver as qce
 
 if __name__ == '__main__':
     number_of_gates = 10
     chromosomes = 10
     generations = 20
-    starting_states_CA = [[0, 0, 0],
-                          [0, 0, 1],
-                          [0, 1, 0],
-                          [0, 1, 1],
-                          [1, 0, 0],
-                          [1, 0, 1],
-                          [1, 1, 0],
-                          [1, 1, 1]]
+    starting_states = [[0, 0, 0],
+                       [0, 0, 1],
+                       [0, 1, 0],
+                       [0, 1, 1],
+                       [1, 0, 0],
+                       [1, 0, 1],
+                       [1, 1, 0],
+                       [1, 1, 1]]
 
     desired_chance_of_one = [0.5, 0.3, 0.4, 0, 0.5, 0.2, 0, 0.9]
 
     best_outcome = 1000
     best_circuit = []
+    circuit_list = []
+
+    #init_circuit =
+
+    #print(init_circuit)
+
+
+    """ def init_first_gen():"""
+    for i in range(chromosomes):
+        circuit_string = qce.CircuitString(number_of_gates*3)
+        circuit_string.generate_gate_string()
+        circuit_list.append(circuit_string)
+    """ *----------------------------* """
+
+    generated_circuit = qce.CircuitGenerator(number_of_gates)
+
+    generated_circuit.run_generation(circuit_list)
 
     for i in range(chromosomes):
         print('Run ' + str(i) + ': ')
         print("---------")
         sum_of_runs = 0
 
-        generated_circuit = EQC.CircuitGenerator(number_of_gates)
+        generated_circuit = qce.CircuitGenerator(number_of_gates)
         generated_circuit.generate_gate_string()
 
         index = 0
 
-        for triplet in starting_states_CA:
+        for triplet in starting_states:
 
             generated_circuit.initialize_initial_states(triplet)
-            generated_circuit.generate_circuit_from_string()
+            generated_circuit.generate_circuit()
             error = generated_circuit.calculate_error(desired_chance_of_one[index])
 
             sum_of_runs = sum_of_runs + error
@@ -56,16 +73,17 @@ if __name__ == '__main__':
         print("generation: " + str(c_num+1))
         for i in range(0, chromosomes):
             sum_of_runs = 0
+
             index = 0
 
-            next_gen_circuit = EQC.CircuitGenerator(number_of_gates)
+            next_gen_circuit = qce.CircuitGenerator(number_of_gates)
             next_gen_circuit.set_gate_string(best_circuit)
             next_gen_circuit.mutate_gate_string()
 
             print('Run mutated ' + str(i) + ': ')
             print("---------")
 
-            for triplet in starting_states_CA:
+            for triplet in starting_states:
                 next_gen_circuit.initialize_initial_states(triplet)
                 next_gen_circuit.generate_circuit_from_string()
                 error = next_gen_circuit.calculate_error(desired_chance_of_one[index])
@@ -89,7 +107,7 @@ if __name__ == '__main__':
 
     print("Final result: " + str(final_outcome))
     print(best_mutated)
-    best_result = EQC.CircuitGenerator(number_of_gates)
+    best_result = qce.CircuitGenerator(number_of_gates)
     best_result.set_gate_string(best_mutated)
     best_result.generate_circuit_from_string()
     best_result.draw_circuit()
--- QuantumCircuitEvolver/QuantumCircuitEvolver/quantum_circuit_evolver.py
+++ QuantumCircuitEvolver/QuantumCircuitEvolver/quantum_circuit_evolver.py
@@ -2,17 +2,70 @@ import math
 import random
 from qiskit import *
 
+class CircuitString(object):
+
+    def __init__(self, string_length):
+        self.gate_string = []
+        self.string_length = string_length
+
+    def generate_gate_string(self):
+        # Returns a randomly generated string representation of given number of qbits
+
+        for i in range(self.string_length):
+            if i % 3 == 0:
+                self.gate_string.append(random.randrange(0, 6))
+            else:
+                self.gate_string.append(random.randrange(0, 3))
+
+        for i in range(0, int(self.string_length / 3)):
+            int_index = i * 3
+
+            if ((self.gate_string[int_index] in [0, 4, 5]) and
+                    self.gate_string[int_index + 1] == self.gate_string[int_index + 2]):
+
+                if self.gate_string[int_index + 1] == 0:
+                    self.gate_string[int_index + 1] = random.randrange(1, 3)
+
+                elif self.gate_string[int_index + 1] == 1:
+                    self.gate_string[int_index + 2] = 0  # TODO - hardcoded
+
+                elif self.gate_string[int_index + 1] == 2:
+                    self.gate_string[int_index + 2] = random.randrange(0, 2)
+        return self.gate_string
+
+    def set_gate_string(self, string_list):
+        self.gate_string = string_list
+
+    def get_gates_string(self):
+        return self.gate_string
+
+    def __repr__(self):
+        return str(self.gate_string) + '\n'
+
 
 class CircuitGenerator(object):
     """ Generates a string of 3*number_of_gates length number representing gate types and position in a quantum
     circuit """
 
     def __init__(self, number_of_gates):
-        self.gate_string = CircuitString.get_gates_string()
+        self.gate_string = []
         self.number_of_gates = number_of_gates
         self.string_length = number_of_gates * 3
         self.circuit = QuantumCircuit(3, 1)
         self.shots = 2048
+
+        self.starting_states = [[0, 0, 0],
+                           [0, 0, 1],
+                           [0, 1, 0],
+                           [0, 1, 1],
+                           [1, 0, 0],
+                           [1, 0, 1],
+                           [1, 1, 0],
+                           [1, 1, 1]]
+
+        self.desired_chance_of_one = [0.5, 0.3, 0.4, 0, 0.5, 0.2, 0, 0.9]
+    def __repr__(self):
+        return str(self.gate_string) + '\n'
 
     def generate_gate_string(self):
         # Returns a randomly generated string representation of given number of qbits
@@ -42,7 +95,7 @@ class CircuitGenerator(object):
 
 
 
-    def generate_circuit_from_string(self):
+    def generate_circuit(self):
         # Parsing integer string and converting it to gates
         self.circuit = QuantumCircuit(3, 1)
 
@@ -72,7 +125,6 @@ class CircuitGenerator(object):
 
         self.circuit.measure(0, 0)
 
-
     def initialize_initial_states(self, triplet):
         if triplet[0] == 1:
             self.circuit.x(0)
@@ -83,6 +135,9 @@ class CircuitGenerator(object):
 
     def run_circuit(self):
         pass
+
+    def clear_string(self):
+        self.gate_string = []
 
     def calculate_error(self, desired_outcome):
 
@@ -101,7 +156,7 @@ class CircuitGenerator(object):
             chance_of_one = 1
             #print("Outcome: " + str(1 - desired_outcome))
             error = 1
-        print("error: " + str(error))
+        #print("error: " + str(error))
         #print()
         return error
 
@@ -128,38 +183,28 @@ class CircuitGenerator(object):
             elif self.gate_string[random_index + 1] == 2:
                 self.gate_string[random_index + 2] = random.randrange(0, 2)
 
-class CircuitString(object):
-
-    def __init__(self):
-        self.gate_string = []
-        self.string_length = len(self.gate_string)
-
-    def generate_gate_string(self):
-        # Returns a randomly generated string representation of given number of qbits
-
-        for i in range(self.string_length):
-            if i % 3 == 0:
-                self.gate_string.append(random.randrange(0, 6))
-            else:
-                self.gate_string.append(random.randrange(0, 3))
-
-        for i in range(0, int(self.string_length / 3)):
-            int_index = i * 3
-
-            if ((self.gate_string[int_index] in [0, 4, 5]) and
-                    self.gate_string[int_index + 1] == self.gate_string[int_index + 2]):
-
-                if self.gate_string[int_index + 1] == 0:
-                    self.gate_string[int_index + 1] = random.randrange(1, 3)
-
-                elif self.gate_string[int_index + 1] == 1:
-                    self.gate_string[int_index + 2] = 0  # TODO - hardcoded
-
-                elif self.gate_string[int_index + 1] == 2:
-                    self.gate_string[int_index + 2] = random.randrange(0, 2)
-
     def set_gate_string(self, string_list):
         self.gate_string = string_list
 
     def get_gates_string(self):
         return self.gate_string
+
+    def clear_circuit(self):
+        self.circuit.data.clear()
+
+    def run_generation(self, circuit_string_list):
+        for circ_str in circuit_string_list:
+            self.run_cromosome_with_init(circ_str)
+
+    def run_cromosome_with_init(self, circuit_string: CircuitString):
+        for i in range(0, len(self.starting_states)):
+            self.clear_circuit()
+            self.clear_string()
+            self.set_gate_string(circuit_string.get_gates_string())
+            self.initialize_initial_states(self.starting_states[i])
+            self.generate_circuit()
+            error = self.calculate_error(self.desired_chance_of_one[i])
+            print("Error state " + str(i+1) + ": " + str(error))
+
+
+
